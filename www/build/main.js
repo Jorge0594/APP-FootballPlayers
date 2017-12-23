6ae12b1de3b077951a068360617cd18be464ad3a
Jorge0594 <jsm0594@gmail.com>
2017-12-23 19:50:49 +0100

    Prototipe welcome slides

--- www/build/main.js
+++ www/build/main.js
@@ -58,7 +58,7 @@ var HomePage = (function () {
     }
     HomePage = __decorate([
         Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["m" /* Component */])({
-            selector: 'page-home',template:/*ion-inline-start:"C:\Users\Jorge\Documents\GitHub\APP-FootballPlayers\src\pages\home\home.html"*/'<ion-header>\n  <ion-navbar>\n    <ion-title>\n      Ionic Blank\n    </ion-title>\n  </ion-navbar>\n</ion-header>\n\n<ion-content padding>\n  The world is your oyster.\n  <p>\n    If you get lost, the <a href="http://ionicframework.com/docs/v2">docs</a> will be your guide.\n  </p>\n</ion-content>\n'/*ion-inline-end:"C:\Users\Jorge\Documents\GitHub\APP-FootballPlayers\src\pages\home\home.html"*/
+            selector: 'page-home',template:/*ion-inline-start:"C:\Users\Jorge\Documents\GitHub\APP-FootballPlayers\src\pages\home\home.html"*/'<ion-content>\n  <ion-slides pager="true" parallax="true" padding>\n\n    <ion-slide>\n      <img src="assets/imgs/ball.png">\n      <h1>Bievenido a la aplicación!</h1>\n    </ion-slide>\n\n    <ion-slide>\n      <img src="assets/imgs/podium.png">\n      <h1>¡Donde tendrás toda la información de tu liga en tu mano!</h1>\n    </ion-slide>\n\n    <ion-slide>\n      <h1>Inicia sesión con tu cuenta</h1>\n      <button ion-button outline small >\n        Acceder\n      </button>\n    </ion-slide>\n\n  </ion-slides>\n</ion-content>'/*ion-inline-end:"C:\Users\Jorge\Documents\GitHub\APP-FootballPlayers\src\pages\home\home.html"*/
         }),
         __metadata("design:paramtypes", [__WEBPACK_IMPORTED_MODULE_1_ionic_angular__["d" /* NavController */]])
     ], HomePage);
