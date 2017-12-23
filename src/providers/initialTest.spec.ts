7a9c31864c1ca378404af4619843c5dd08d2d1b1
Jorge0594 <jsm0594@gmail.com>
2017-12-23 18:57:56 +0100

    Initialises on HomePage UNIT Test

--- src/providers/initialTest.spec.ts
+++ src/providers/initialTest.spec.ts
@@ -1,5 +1,47 @@
+import { MyApp } from '../app/app.component';
+import { HomePage } from '../pages/home/home';
+import { ErrorHandler} from '@angular/core';
+import { Component } from '@angular/core/src/metadata/directives';
+import { ComponentFixture, TestBed, async } from '@angular/core/testing';
+import { createComponentFactory } from '@angular/core/src/view/refs';
+import { createComponentView } from '@angular/core/src/view/view';
+import { IonicModule,IonicErrorHandler} from 'ionic-angular';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+let component : MyApp;
+let fixture : ComponentFixture<MyApp>
+
 describe('Initial test',()=>{
-    it('Test sum',()=>{
-        expect(1+1).toBe(2);
+    beforeEach(async()=>{
+        TestBed.configureTestingModule({
+                declarations:[MyApp],
+                providers:[
+                    StatusBar,
+                    SplashScreen,
+                    {provide: ErrorHandler, useClass: IonicErrorHandler}
+                ],
+                imports:[
+                    IonicModule.forRoot(MyApp)
+                ]
+        }).compileComponents();
+    });
+
+    beforeEach(()=>{
+        fixture = TestBed.createComponent(MyApp);
+        component = fixture.componentInstance;
+    });
+
+    afterEach(()=>{
+        fixture.destroy();
+        component = null;
+    });
+
+    it('is created',()=>{
+        expect(fixture).toBeTruthy();
+        expect(component).toBeTruthy();
+    });
+
+    it('Initialises with a root page of HomePage',()=>{
+        expect(component['rootPage']).toBe(HomePage);
     });
 });
