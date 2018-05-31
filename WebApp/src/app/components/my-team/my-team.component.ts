cf4d6c8f0d68f1a9ffef9faf05812543197c9ae1
Jorge0594 <jsm0594@gmail.com>
2018-05-31 21:10:31 +0200

    User team visualization added

--- WebApp/src/app/components/my-team/my-team.component.ts
+++ WebApp/src/app/components/my-team/my-team.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../../services/user.service';
+import { TeamDataService } from '../../services/team-data.service';
 
 @Component({
   selector: 'app-my-team',
